5650e71f8122827193d76b2591a9b61656cdd501
Rodrigo Baptista <rmb.tito@gmail.com>
2017-05-14 10:09:27 -0300

    Alteracao de conteudo da pagina

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App />,//<- Modo simplificado de escrever => Document.createElement(App),
   document.getElementById('root')
 );
